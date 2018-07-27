dea9b3b7be0ff28a5fbb9d619fe51d42903e9d6a
Karen Gonzalez <ksanchez189@gmail.com>
2018-07-27 09:13:42 -0400

    adding tests for webpages endpoint

--- performline/products/web/tests/webpage_test.py
+++ performline/products/web/tests/webpage_test.py
@@ -25,23 +25,81 @@
 
 from __future__ import unicode_literals
 import unittest
+from ..models from Webpage
 from ....testing import client
 
 
-# class TestPages(unittest.TestCase):
-#     def setUp(self):
-#         self.client = client()
+class TestPages(unittest.TestCase):
+    def setUp(self):
+        self.client = client()
 
-#     def test_get_all_pages(self):
-#         pages = list(self.client.webpages())
-#         self.assertIsInstance(pages, list)
-#         self.assertTrue(len(pages) > 0)
+    def test_get_all_pages(self):
+        pages = list(self.client.webpages())
+        self.assertIsInstance(pages, list)
+        self.assertTrue(len(pages) = 3)
 
-#         page = pages[0]
+        page = pages[0]
 
-#         self.assertEqual(page.brand, 'A. Foo Industries')
-#         self.assertEqual(page.creation_date, '2016-09-13T11:00:00')
-#         self.assertEqual(page.grade, 'GOOD')
-#         self.assertEqual(page.origin, 'Known')
-#         self.assertEqual(page.score, 50.0)
-#         self.assertEqual(page.traffic_source, 'tester')
+        self.assertIsInstance(page, Webpage)
+        self.assertEqual(page.Id, 10)
+        self.assertEqual(page.Type, 'web')
+        self.assertEqual(page.Score, 80)
+        self.assertEqual(page.TrafficSourceId, 1)
+        self.assertEqual(page.CampaignId, 3)
+        self.assertEqual(page.BrandId, 12)
+        self.assertEqual(page.CompanyId, 10)
+        self.assertEqual(page.CreatedAt, '2018-7-24T00:00:00')
+        self.assertEqual(page.Url, "https://www.wsj.com")
+        self.assertEqual(page.CreatedAt, '2018-7-25T00:00:00')
+
+    def test_webpage_endpoint_access(self):
+        # Company token does not have access to page_id = 13  
+        page = list(self.client.webpages(13))
+
+        self.assertIsInstance(page, list)
+        self.assertTrue(len(page) == 0)
+
+    def test_get_webpage_by_id(self):
+        #Wil test that all attributes are returned for page id = 11
+        page = self.client.webpages(11)
+
+        self.assertIsInstance(page, Webpage)
+
+        self.assertEqual(page.Id, 11)
+        self.assertEqual(page.Type, 'web')
+        self.assertEqual(page.Score, 70)
+        self.assertEqual(page.TrafficSourceId, 1)
+        self.assertEqual(page.CampaignId, 3)
+        self.assertEqual(page.BrandId, 12)
+        self.assertEqual(page.CompanyId, 10)
+        self.assertEqual(page.CreatedAt, '2018-7-24T00:00:00')
+        self.assertEqual(page.Url, "https://www.washingtonpost.com")
+        self.assertEqual(page.CreatedAt, '2018-7-25T00:00:00')
+
+    def test_get_webpage_in(self):
+        #Will test parameters
+        pages = list(self.client.webpages()) # this has 3 pages
+
+        pages_in_limit = list(self.client.webpages(limit=2))
+
+        self.assertEqual(len(pages_in_limit, 2))
+        self.assertEqual(pages_in_limit[0], 10)
+        self.assertEqual(pages_in_limit[1], 11)
+
+        pages_in_offset = list(self.client.webpages(offset=1))
+
+        self.assertEqual(len(pages_in_offset, 2))
+        self.assertEqual(pages_in_limit[0], 11)
+        self.assertEqual(pages_in_limit[1], 12)
+
+        pages_in_campaign = list(self.client.webpages(campaign=3))
+
+        self.assertEqual(len(pages_in_campaign, 2))
+        self.assertEqual(pages_in_limit[0], 10)
+        self.assertEqual(pages_in_limit[1], 11)
+
+        pages_in_brand = list(self.client.webpages(brand=12))
+
+        self.assertEqual(len(pages_in_brand, 2))
+        self.assertEqual(pages_in_limit[0], 11)
+        self.assertEqual(pages_in_limit[1], 12)
